b747a7cfc630daa15734dbbe4678d46939409a65
Julia <jerlo180908@gmail.com>
2025-09-01 10:39:35 -0300

    Parte 1 - Testando GET com Filtros

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import dados from "./src/data/dados.js"
-const { bruxos } = dados;
+import res from "express/lib/response.js";
+const { bruxos, varinhas, pocoes, animais } = dados;
+
 
 // Cria aplicação com Express e configura para aceitar JSON
 const app = express();
@@ -18,7 +20,7 @@ app.get("/", (req, res) => {
 });
 
 // Aqui vão todas suas Rotas
-// Query Parameters no Node.js - API de Hogwarts
+// Rota de bruxos com filtro (Query Params)
 app.get('/bruxos', (req, res) => {
     const { casa, ano, especialidade, nome } = req.query;
     let resultado = bruxos;
@@ -62,7 +64,7 @@ app.post("/bruxos", (req, res) => {
         ano: parseInt(ano),
         varinha: varinha || "Ainda não definida!",
         mascote: mascote || "Ainda não definido!",
-        patrono: patrono || "AInda não descoberto!",
+        patrono: patrono || "Ainda não descoberto!",
         especialidade: especialidade || "Em desenvolvimento...",
         vivo: vivo
     }
@@ -74,6 +76,61 @@ app.post("/bruxos", (req, res) => {
         sucess: true,
         message: "Novo bruxo adicionado a Hogwarts!",
         data: novoBruxo
+    });
+});
+
+//Rota de varinhas com filtro
+app.get('/varinhas', (req, res) => {
+    const { material, nucleo } = req.query;
+    let resultado = varinhas;
+
+    if (material) {
+        resultado = resultado.filter(v => v.material.toLowerCase().includes(material.toLowerCase()));
+    }
+
+    if (nucleo) {
+        resultado = resultado.filter(v => v.nucleo.toLowerCase().includes(nucleo.toLowerCase()));
+    }
+
+    res.status(200).json({
+        total: resultado.length,
+        data: resultado
+    });
+});
+
+app.get("/pocoes", (req, res) => {
+    const {nome, efeito } = req.query;
+    let resultado = pocoes;
+
+    if (nome) {
+        resultado = resultado.filter(p => p.nome.toLowerCase().includes(nome.toLowerCase()));
+    }
+
+    if (efeito) {
+        resultado = resultado.filter(p => p.efeito.toLowerCase().includes(efeito.toLowerCase()));
+    }
+
+    res.status(200).json({
+        total: resultado.length,
+        data: resultado
+    });
+});
+
+app.get("/animais", (req, res) => {
+    const {nome, tipo } = req.query;
+    let resultado = animais;
+
+    if (nome) {
+        resultado = resultado.filter(a => a.nome.toLowerCase().includes(nome.toLowerCase()));
+    }
+
+    if (tipo) {
+        resultado = resultado.filter(a => a.tipo.toLowerCase().includes(tipo.toLowerCase()));
+    }
+
+    res.status(200).json({
+        total: resultado.length,
+        data: resultado
     });
 });
 
